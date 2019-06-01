f67ce581a5d1f38d259e8e0b015df24e94240e0b
Frank Navarro <fnavarro7777@gmail.com>
2019-06-01 12:29:01 -0700

    Added count down bar underneath camera

--- photobooth/gui/camerapage.py
+++ photobooth/gui/camerapage.py
@@ -92,13 +92,16 @@ class CameraFrame(tk.Frame):
 
         self.controller = controller
 
+        # Count down bar size
+        countBarHeight = 10
+
         # Inherit sizing from controller
         self.cameraResolution = controller.photostrip.imageSize
         self.size = controller.containerSize
 
         # Get the height of the camera window frame
         camY = controller.textHeight # Also defines the padding for the top/bottom
-        camH = self.size[1]
+        camH = self.size[1] - countBarHeight
 
         # Get the width of the camera window frame
         camW = int(camH * self.cameraResolution[0] / self.cameraResolution[1])
@@ -106,12 +109,13 @@ class CameraFrame(tk.Frame):
 
         # Coordinates for camera on the screen
         cameraPadding = 10
-        self.cameraPosition = (camX, camY, camW, camH) # [X, Y, Width, Height]
+        self.cameraContainer = (camX, camY, camW, camH) # [X, Y, Width, Height]
         self.cameraCoordinates = (camX + cameraPadding, camY + cameraPadding, camW - cameraPadding*2, camH - cameraPadding*2)
-        print("Camera Size: {}x, {}y, {}w, {}h".format(camX, camY, camW, camH))
+        print("Camera Size: {}x, {}y, {}w, {}h".format(self.cameraCoordinates[0], self.cameraCoordinates[1], self.cameraCoordinates[2], self.cameraCoordinates[3]))
 
         # Get the relative camera position values from the parent
         self.cameraViewSize = (self.cameraCoordinates[2], self.cameraCoordinates[3])
+        print("CameraViewSize: {}w, {}h".format(self.cameraViewSize[0], self.cameraViewSize[1]))
 
         # Start the camera service
         self.camera = Camera(self.cameraCoordinates, self.cameraResolution)
@@ -124,7 +128,7 @@ class CameraFrame(tk.Frame):
 
 
         #Initialize the count down sequence
-        self.maxCountDown = 1
+        self.maxCountDown = 5
 
         # Keep references here of the the top and bottom text of the parent for future use
         self.topText = controller.topText
@@ -132,17 +136,27 @@ class CameraFrame(tk.Frame):
 
         # Size the contents in the camera frame properly
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1, minsize=self.cameraPosition[0])
-        self.grid_columnconfigure(1, weight=1, minsize=self.cameraPosition[3])
-        self.grid_columnconfigure(2, weight=1, minsize=self.cameraPosition[0])
+        self.grid_columnconfigure(0, weight=1, minsize=self.cameraContainer[0])
+        self.grid_columnconfigure(1, weight=1, minsize=self.cameraContainer[3])
+        self.grid_columnconfigure(2, weight=1, minsize=self.cameraContainer[0])
 
         # Padding to the left of the camera
         self.cameraPad1 = tk.Frame(self, bg=self["bg"])
         self.cameraPad1.grid(row=0, column=0, sticky="nsew")
 
-        # A Label where the camera will sit over and where pictures will be displayed
-        self.picture = tk.Label(self, width=self.cameraPosition[2], height=self.cameraPosition[3], bg="#F8F8FF")
-        self.picture.grid(row=0, column=1, sticky="nsew")
+        # A Frame where the camera will sit over and where pictures will be displayed
+        offWhiteColor = "#F8F8FF"
+        self.pictureFrame = tk.Frame(self, width=self.cameraContainer[2], bg=self["bg"])
+        self.pictureFrame.grid(row=0, column=1, sticky="nsew")
+        self.pictureFrame.grid_rowconfigure(0, weight=1)
+        self.pictureFrame.grid_columnconfigure(0, weight=1)
+
+        self.picture = tk.Label(self.pictureFrame, width=self.cameraContainer[2], height=self.cameraContainer[3], bg=offWhiteColor)
+        self.picture.grid(row=0, column=0, sticky="nsew")
+
+        self.countDownBar = CountDownBar(self.pictureFrame, maxTime=self.maxCountDown-1, height=countBarHeight)
+        self.countDownBar.grid(row=1, column=0, sticky="nsew")
+
 
         # Padding to the right of the camera
         self.cameraPad2 = tk.Frame(self, bg=self["bg"])
@@ -155,6 +169,7 @@ class CameraFrame(tk.Frame):
     # Add a picture to the screen
     def updatePicture(self, img):
         self.picture.configure(image=img)
+        print("Displayed IMG Size: {}w, {}h".format(img.width(), img.height()))
 
     # Remove the picture from the screen
     def hidePicture(self):
@@ -176,7 +191,7 @@ class CameraFrame(tk.Frame):
         # Hide the picuture behind the camera
         # I delay the hide so that the screen doesn't look like it 
         # flashes with everything changing all at once
-        self.after(500, self.hidePicture)
+        self.after(250, self.hidePicture)
 
         if self.photoNumber < self.maxPhotos:
             # Update the count down number and photo number
@@ -187,7 +202,6 @@ class CameraFrame(tk.Frame):
             # Because that would mean that the camera is already running
             if self.photoNumber != 1:
                 self.camera.start()
-                print("Restart Camera")
 
 
             # Display which photo number this is 
@@ -196,14 +210,14 @@ class CameraFrame(tk.Frame):
             # Start counting down 
             self.after(1000, self.updateCountDown)
         else:
-            self.topText.hideText()
-            self.botText.hideText()
             self.controller.load_frame(PrintingPage)
             self.controller.show_frame(PrintingPage)
 
 
     # Decrement the count down number by 1
     def updateCountDown(self):
+        if self.currentCountDown == self.maxCountDown:
+            self.countDownBar.start()
         # Only decrement if less than 0
         if self.currentCountDown > 0:
             self.botText.updateText(self.currentCountDown)
@@ -211,8 +225,7 @@ class CameraFrame(tk.Frame):
             self.after(1000, self.updateCountDown)
         # If count down is at zero then take a photo
         else:
-            self.botText.updateText("SNAP")
-            self.after(500, self.takePhoto)
+            self.takePhoto()
 
 
     # Process to take a photo and display it for the user to see
@@ -395,32 +408,35 @@ class CountDownBar(tk.Canvas):
         else:
             self.maxSeconds = 5
 
+        # Calculate the time
         self.time = int(self.maxSeconds * 1000)
         self.updateTime = 5
+
+        # Create the bar we will be updating
+        self.bar = self.create_rectangle(0, 0, 0, 0, fill=self.color, outline="")
 
 
     def start(self):
         # Get the size of the count down line
         self.update()
-        self.width = self.winfo_width()
+        self.barWidth = self.winfo_width()
         # Create a rectangle that spans the width of the canvas
-        self.bar = self.create_rectangle(0, 0, self.width, self.height, fill=self.color, outline="")
-        # Just incase reset the background color
-        self.configure(bg=self.backColor)
+        self.coords(self.bar, (0, 0, self.barWidth, self.height))
+        self.update()
         # Calculate the amount we need to adjust the width with every update call
-        self.updateSize = self.updateTime * self.width / self.time
+        self.updateSize = self.updateTime * self.barWidth / self.time
         # Start counting down
         self.after(self.updateTime, self.updateBar)
     
     def updateBar(self):
         # As long as we have more time decrement the bars length
         if self.time >= 0:
-            self.width -= self.updateSize
+            self.barWidth -= self.updateSize
             self.time -= self.updateTime
-            self.coords(self.bar, (0, 0, self.width, self.height))
+            self.coords(self.bar, (0, 0, self.barWidth, self.height))
             self.update()
             self.after(self.updateTime, self.updateBar)
 
         # Once time is out reset out time variable
         else:
-            self.time = self.maxSeconds
+            self.time = int(self.maxSeconds * 1000)
