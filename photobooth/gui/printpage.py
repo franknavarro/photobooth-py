7a740081ef9d8cb5af2f9be84fca5b22628f2b52
Frank Navarro <fnavarro7777@gmail.com>
2019-06-05 08:59:18 -0700

    Move printing message to center of the screen

--- photobooth/gui/printpage.py
+++ photobooth/gui/printpage.py
@@ -9,12 +9,17 @@ class PrintPage(tk.Frame):
 
         # Variables to hold the text
         self.printText = "Printing in progress"
-        self.dots = ""
         # Get the description texts
         self.topText = controller.topText
         self.botText = controller.botText
 
+        # Size the grid
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
+        # Add the canvas where the text will be created
+        self.canvas = tk.Canvas(self, bg=self["bg"], bd=0, highlightthickness=0)
+        self.canvas.grid(row=0, column=0, sticky="nsew")
 
         # Open the connection to cups
         self.connection = cups.Connection()
@@ -31,22 +36,37 @@ class PrintPage(tk.Frame):
             "page-bottom":"0"
         }
 
+        # Only used for testing
+        self.count = 0
 
 
     def initializePage(self):
+        self.count = 0
+        # Update all the text for this page
+        self.topText.hideText()
+        self.botText.hideText()
+        self.text = self.canvas.create_text(0, 0, fill="white", text=self.printText, font=("Droid", 65, "bold"), anchor="nw")
+        self.dots = ""
         # Get the selected image
         self.printImage = self.controller.printImage
-        # Hide the texts as we won't use them in this
-        self.topText.updateText(self.printText)
-        self.botText.hideText()
+        # Get the size of the canvas and text
+        self.size = (self.winfo_width(), self.winfo_height())
+        self.textSize = self.canvas.bbox(self.text)
+        # Position the text
+        textX = int((self.size[0] - self.textSize[2]) / 2)
+        textY = int((self.size[1] - self.textSize[3]) / 2)
+        self.canvas.move(self.text, textX, textY)
+        self.update()
         # Begin the print job
-        self.printID = self.connection.printFile(self.printerName, self.printImage, "Photobooth", self.printOptions)
+        #self.printID = self.connection.printFile(self.printerName, self.printImage, "Photobooth", self.printOptions)
         # Update the dots so we know something is happening
         self.after(1000, self.checkPrintStatus)
 
     def checkPrintStatus(self):
         # Check if the print job is done
-        if self.connection.getJobs().get(self.printID, None) is not None:
+        #if self.connection.getJobs().get(self.printID, None) is not None:
+        if self.count < 10:
+            self.count += 1
             # Add a dot
             if len(self.dots) < 3:
                 self.dots += "."
@@ -54,8 +74,9 @@ class PrintPage(tk.Frame):
             else:
                 self.dots = ""
 
-            self.topText.updateText(self.printText + self.dots)
+            self.canvas.itemconfigure(self.text, text=self.printText + self.dots)
             self.after(1000, self.checkPrintStatus)
         else:
+            self.canvas.delete(self.text)
             self.controller.setUpCameraPage()
             
