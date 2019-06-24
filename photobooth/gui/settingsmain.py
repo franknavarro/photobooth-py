8ed8e8b0d11e0f3f5896681c97d04b9bb81d25fb
Frank Navarro <fnavarro7777@gmail.com>
2019-06-24 15:53:11 -0700

    Added tab pages templates

--- photobooth/gui/settingsmain.py
+++ photobooth/gui/settingsmain.py
@@ -1,31 +1,37 @@
 import tkinter as tk
 
+from .settings.apperancetab import ApperanceTab
+from .settings.interactionstab import InteractionsTab
+from .settings.photostriptab import PhotostripTab
+
 class SettingsMain(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg="#ffffff")
+        tk.Frame.__init__(self, parent, bg=parent["bg"])
 
         self.controller = controller
 
         # Initialize the grid for the page
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(1, weight=10)
+
+        self.tabs = (ApperanceTab, InteractionsTab, PhotostripTab)
 
         # Set up the container where the tabs will be displayed
         self.container = tk.Frame(self, bg=self["bg"])
-        self.container.grid(row=1, column=0, columnspan=3, sticky="nsew")
+        self.container.grid(row=1, column=0, columnspan=len(self.tabs), sticky="nsew")
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
 
 
         # Initialize the different settings tabs
-        #self.frames = {}
-        #for tab in (ApperanceTab, InteractionsTab, PhotostripTab):
-            #frame = tab(self.container, self)
-            #self.frames[tab] = frame
-            #frame.grid(row=0, column=0, stick="nsew")
+        self.frames = {}
+        for index, tab in enumerate(self.tabs):
+            # Initialize the tab column that this will be in
+            self.grid_columnconfigure(index, weight=1)
+            # Initialize the page assosiated with the tab
+            frame = tab(self.container, self)
+            self.frames[tab] = frame
+            frame.grid(row=0, column=0, stick="nsew")
 
 
     # Perform any initial configurations
@@ -34,6 +40,7 @@ class SettingsMain(tk.Frame):
         self.configure(cursor='arrow')
         # Bind the close key
         self.bind('x', self.close)
+        self.show_frame(self.tabs[0])
 
     # Function that will show the correct tab page
     def show_frame(self, cont):
