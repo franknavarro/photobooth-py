e7d54d01077511fe3e72b42cda385f0d1365d341
Frank Navarro <fnavarro7777@gmail.com>
2019-07-10 14:02:09 -0700

    Added a font size box

--- photobooth/gui/components/fontentry.py
+++ photobooth/gui/components/fontentry.py
@@ -10,12 +10,22 @@ class FontEntry(tk.Frame):
         # Auto size grid
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
+
+        # Make our bindings to whatever our callback funciton is
+        if(callback):
+            self.callback = callback
+        else:
+            self.callback = self.getFont
+
+        # Padding
+        pad = globe['fieldPadding']
+        halfPad = pad/2
         
         # Initialize the header text
         if(title):
             self.headerText = title
             self.header = tk.Label(self, font=darksetting['font'], text=self.headerText, bg=self["bg"], fg=darksetting['fg'])
-            self.header.grid(row=0, column=0, pady=(0, globe['fieldPadding']/2))
+            self.header.grid(row=0, column=0, pady=(0, halfPad))
         else:
             self.headerText = "Font"
 
@@ -35,14 +45,33 @@ class FontEntry(tk.Frame):
             if( fontFam == self.fontfamily ):
                 self.fontindex = index
         self.fontlist.selection_set(first=0)
-        self.fontlist.grid(row=1, column=0, sticky="nsew")
+        self.fontlist.grid(row=1, column=0, sticky="nsew", pady=(halfPad, halfPad))
+        self.fontlist.bind('<Button-1>', self.callback)
 
-        # Make our bindings to whatever our callback funciton is
-        if(callback):
-            self.fontlist.bind('<Button-1>', callback)
+        # Create a variable to hold the value of the fontsize
+        self.fontsize = tk.StringVar()
+        self.fontsize.set(fontsize)
+        self.fontsize.trace('w', self.callback)
+        # Create the font size entry field
+        fontvalid = (self.register(self.sizeValidation), '%P', '%S')
+        self.sizeEntry = tk.Entry(self, font=darksetting['font'], width=3, relief="flat")
+        self.sizeEntry.insert(tk.END, self.fontsize.get())
+        self.sizeEntry.config(validate="key", validatecommand=fontvalid)
+        self.sizeEntry.grid(row=2, column=0, sticky="nsew", pady=(halfPad, 0), padx=(0, halfPad))
+
+
+    def sizeValidation(self, textafter, textinserted):
+        # Only allow digit entries 
+        if textinserted.isdigit():
+            self.fontsize.set(textafter)
+            return True
+        else:
+            return False
+        
 
     def getFont(self):
         fontFamily = self.fontlist.get(tk.ACTIVE)
-        return (fontFamily, 65, "bold")
+        fontSize = 0 if self.fontsize.get() == "" else self.fontsize.get()
+        return (fontFamily, fontSize, "bold")
 
 
